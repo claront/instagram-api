6a20eaea9d3beeba210484c1afd50b113e07e50b
Claron Twitchell <claront@gmail.com>
2015-03-02 17:46:43 -0700

    trying a call to MMR, and added a README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Experimentation with Instagram and MapMyRun APIs in [AngularJS](https://angularjs.org)
+
+Experimenting with the [Switchable-Grid](http://tutorialzine.com/2013/08/learn-angularjs-5-examples/), example 5, which shows how to call the Instagram API, then morphing the code to call the MapMyRun API.
+
+This is side exercise in figuring out how to build a web app that will [convert mapmyfitness workouts to gpx format.](https://github.com/claront/mapmyfitness-gpx)
+
--- script.js
+++ script.js
@@ -12,11 +12,13 @@ app.factory('instagram', function($resource){
 			// The ngResource module gives us the $resource service. It makes working with
 			// AJAX easy. Here I am using a client_id of a test app. Replace it with yours.
 
+
 			// var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
 			// 	client_id: '642176ece1e7445e99244cec26f4de1f'
-			var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
-				client_id: '642176ece1e7445e99244cec26f4de1f'
-			},{
+			// },
+			var api = $resource('http://www.mapmyrun.com/vxproxy/v7.0/workout/782830271/?field_set=time_series&callback=JSON_CALLBACK',
+				{},
+			{
 				// This creates an action which we've chosen to name "fetch". It issues
 				// an JSONP request to the URL of the resource. JSONP requires that the
 				// callback=JSON_CALLBACK part is added to the URL.
